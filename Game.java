77e3afc5f467262eafd016f6a170413285ad4d15
jrhemmin <jrhemmin@asu.edu>
2017-04-27 09:10:42 -0700

    Timothy, Music on/off button

--- Game.java
+++ Game.java
@@ -19,16 +19,47 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+import com.neet.DiamondHunter.Manager.JukeBox;
+
 public class Game {
 	
+	public static boolean enabled = true;
 	public static void main(String[] args) {
 		
+		
 		JFrame window = new JFrame("Diamond Hunter");
-		JMenuBar mb = new JMenuBar();
+		final JMenuBar menuBar = new JMenuBar();
 		JMenuItem menu = new JMenuItem("Help"); //adding help menu
 		JMenuItem score = new JMenuItem("Scoring"); //adding scoring menu
-		mb.add(score);
-		mb.add(menu);
+		final JMenuItem music = new JMenuItem("Music On");
+		menuBar.add(score);
+		menuBar.add(menu);
+		
+		menuBar.add(menu);
+		menuBar.add(music);
+		
+		music.addActionListener(new ActionListener() { 
+			public void actionPerformed(final ActionEvent event){
+			
+				
+			
+				if(enabled == true){
+				JukeBox.setVolume("music1", -80);
+				music.setText("Music off");
+				enabled = false;
+				}
+				
+				else{
+					JukeBox.setVolume("music1", -10);
+
+					music.setText("Music on");
+					enabled = true;
+				}
+				
+			}
+			
+		});
+		
 		menu.addActionListener(new ActionListener() {  
 			public void actionPerformed(ActionEvent ev){
 				
@@ -54,11 +85,11 @@ public class Game {
 				public void actionPerformed(ActionEvent ev){
 				JFrame scoreWindow = new JFrame("Scoring");
 				
-				JLabel label1 = new JLabel("<html><br> Scoring Ranks:<br><br>" 
-						+ " Speed Demon: Under 2 Minutes <br>"
-						+ " Adventurer: Under 3 Minutes <br>"
-						+ " Beginner: Under 4 Minutes <br>"
-						+ " Bumbling Idiot: Over 4 Minutes </html>");
+				JLabel label1 = new JLabel("<html><br>  Scoring Ranks:<br><br>" 
+						+ "  Speed Demon: Under 2 Minutes <br>"
+						+ "  Adventurer: Under 3 Minutes <br>"
+						+ "  Beginner: Under 4 Minutes <br>"
+						+ "  Bumbling Idiot: Over 4 Minutes </html>");
 
 				
 				label1.setFont(new Font("TimesRoman",Font.PLAIN, 20));
@@ -70,7 +101,7 @@ public class Game {
 		});
 
 		window.add(new GamePanel());
-		window.add(mb, BorderLayout.NORTH);
+		window.add(menuBar, BorderLayout.NORTH);
 
 		window.setResizable(false);
 		window.pack();
